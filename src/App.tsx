160d8cfaf23bfe06931b2e5e11d07931c5743026
Oleg Orlov <o.orlov@outlook.com>
2023-02-12 21:23:12 +0300

    Updated button state depends on inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { FC, useState } from 'react';
+
 import Form from './Form';
 
 const App: FC = () => {
--- src/Form.tsx
+++ src/Form.tsx
@@ -1,27 +1,50 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
 
 const Form: FC = () => {
+  const [login, setLogin] = useState<string>('');
+  const [repo, setRepo] = useState<string>('');
+  const [disabled, setDisabled] = useState<boolean>(true);
+
+  useEffect(() => {
+    setDisabled(!!(login && repo));
+  }, [login, repo]);
+
   return (
     <form>
       <label>
         Login
         <br />
-        <input type="text" name="login" />
+        <input
+          type="text"
+          name="login"
+          onInput={e => setLogin((e.target as HTMLInputElement).value)}
+        />
       </label>
       <br />
       <label>
         Repository
         <br />
-        <input type="text" name="repo" />
-        </label>
+        <input
+          type="text"
+          name="repo"
+          onInput={e => setRepo((e.target as HTMLInputElement).value)}
+        />
+      </label>
       <br />
       <label>
-        Blacklist
+        Reviewer Blacklist
         <br />
-        <input type="text" name="blacklist" />
-        </label>
+        <input
+          type="text"
+          name="blacklist"
+        />
+      </label>
       <br />
-      <input type="button" value="Search" />
+      <input
+        type="button"
+        value="Search Reviewer"
+        disabled={!disabled}
+      />
     </form>
   );
 }
