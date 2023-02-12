379a68bf2db1d4ca40ec82474f2e57f9189b2ebd
Oleg Orlov <o.orlov@outlook.com>
2023-02-12 13:27:33 +0300

    Show/hide form using button

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
-function App() {
+import { FC, useState } from 'react';
+import Form from './Form';
+
+const App: FC = () => {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div>
+      <button onClick={() => setVisible(!visible)} type="button">Settings</button>
+      {visible && <Form />}
     </div>
   );
 }
