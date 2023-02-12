83869dd50361dd7a38c5c7745ebdb3e43583757e
Oleg Orlov <o.orlov@outlook.com>
2023-02-12 13:59:30 +0300

    Render form

--- src/Form.tsx
+++ src/Form.tsx
@@ -2,9 +2,27 @@ import { FC } from 'react';
 
 const Form: FC = () => {
   return (
-    <div>
-      Form
-    </div>
+    <form>
+      <label>
+        Login
+        <br />
+        <input type="text" name="login" />
+      </label>
+      <br />
+      <label>
+        Repository
+        <br />
+        <input type="text" name="repo" />
+        </label>
+      <br />
+      <label>
+        Blacklist
+        <br />
+        <input type="text" name="blacklist" />
+        </label>
+      <br />
+      <input type="button" value="Search" />
+    </form>
   );
 }
 
